466bf5b8b55e56f7354bf363596f6fcbc3d7f369
Esteban Quintero <albeesteban19@gmail.com>
2023-04-23 16:37:34 -0500

    actualizacion y arreglo de clase 21/04/2023

--- PubliUcoCrossCutting/src/co/edu/uco/publiuco/crosscutting/utils/UtilUUID.java
+++ PubliUcoCrossCutting/src/co/edu/uco/publiuco/crosscutting/utils/UtilUUID.java
@@ -2,13 +2,13 @@ package co.edu.uco.publiuco.crosscutting.utils;
 
 import java.util.UUID;
 
-public final class UtillUUID {
+public final class UtilUUID {
 	
 	public static final String DEFAULT_UUID_AS_STRING = "00000000-0000-0000-000000000000";
 	public static final UUID DEFAULT_UUID = generateUUIDFromString(DEFAULT_UUID_AS_STRING);
 	private static final String UUID_RE = "[a-f0-9]{8}([-][a-)" 
 	
-	private UtillUUID() {
+	private UtilUUID() {
 		super();
 	}
 	
--- PubliUcoDTO/src/co/edu/uco/publiuco/dto/EstadoTipoRelacionInstitucionDTO.java
+++ PubliUcoDTO/src/co/edu/uco/publiuco/dto/EstadoTipoRelacionInstitucionDTO.java
@@ -2,6 +2,9 @@ package co.edu.uco.publiuco.dto;
 
 import java.util.UUID;
 
+import co.edu.uco.publiuco.crosscutting.utils.UtilText;
+import co.edu.uco.publiuco.crosscutting.utils.UtilUUID;
+
 public final class EstadoTipoRelacionInstitucionDTO {
 	
 	private UUID identificador;
@@ -10,25 +13,29 @@ public final class EstadoTipoRelacionInstitucionDTO {
 	
 	public EstadoTipoRelacionInstitucionDTO() {
 		super();
-		setIdentificador(identificador);
-		setNombre("");
-		setDescripcion("");
+		setIdentificador(UtilUUID.DEFAULT_UUID);
+		setNombre(UtilText.getUtilText().getDefaultValue());
+		setDescripcion(UtilText.getUtilText().getDefaultValue());
 	}
 	
-	public EstadoTipoRelacionInstitucionDTO(final UUID identificador, String nombre, String descripcion) {
+	public EstadoTipoRelacionInstitucionDTO(final UUID identificador, final String nombre, final String descripcion) {
 		super();
-		setIdentificador(identificador);
-		setNombre("");
-		setDescripcion("");
+		setIdentificador(UtilUUID.DEFAULT_UUID);
+		setNombre(UtilText.getUtilText().getDefaultValue());
+		setDescripcion(UtilText.getUtilText().getDefaultValue());
 	}
+	
+	public static EstadoTipoRelacionInstitucionDTO create() {
+		return new EstadoTipoRelacionInstitucionDTO();
+	}
+	
 	
 	public final UUID getIdentificador() {
 		return identificador;
 	}
 	
 	public final EstadoTipoRelacionInstitucionDTO setIdentificador(final UUID identificador) {
-		this.identificador = identificador;
-		
+		this.identificador = UtilUUID.getDefault(identificador);
 		return this;
 		
 	}
@@ -39,11 +46,8 @@ public final class EstadoTipoRelacionInstitucionDTO {
 	
 	public final EstadoTipoRelacionInstitucionDTO setNombre(final String nombre) {
 		
-		this.nombre = "";
+		this.nombre = UtilText.getUtilText().applyTrim(nombre);
 		
-		if (nombre != null ) {
-			this.nombre = nombre.trim();
-		}
 		return this;
 	}
 	
@@ -53,13 +57,16 @@ public final class EstadoTipoRelacionInstitucionDTO {
 	
 	public final EstadoTipoRelacionInstitucionDTO setDescripcion(final String descripcion) {
 		
-		this.descripcion = "";
+		this.descripcion = UtilText.getUtilText().applyTrim(descripcion);
 		
-		if (descripcion != null ) {
-			this.descripcion = descripcion.trim();
-		}
 		return this;
 	}
 
+	@Override
+	public String toString() {
+		return "EstadoTipoRelacionInstitucionDTO [identificador=" + identificador + ", nombre=" + nombre
+				+ ", descripcion=" + descripcion + "]";
+	}
+	
 	
 }
